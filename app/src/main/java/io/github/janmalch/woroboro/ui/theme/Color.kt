a42f9a179d8851aec1416771844e901f953e1111
JanMalch <25508038+JanMalch@users.noreply.github.com>
2023-10-31 18:27:10 +0100

    feat: add basic colors and App icons
    Icons were created with https://icon.kitchen/

--- app/src/main/java/io/github/janmalch/woroboro/ui/theme/Color.kt
+++ app/src/main/java/io/github/janmalch/woroboro/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package io.github.janmalch.woroboro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Teal500 = Color(0xFF009688)
+val Pink = Color(0xFFFF2A8F)
+val BlueishGrey = Color(0xFF7894AE)
