b6d4f0574ed6474e7b5c940f8b813179f15cc9f3
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-05-26 21:25:02 +0200

    chore: setup formatting with spotless and ktfmt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.google.dagger.hilt.android) apply false
     alias(libs.plugins.com.google.devtools.ksp) apply false
+    alias(libs.plugins.spotless)
 }
 true // Needed to make the Suppress annotation work for the plugins block
 buildscript {
@@ -13,4 +14,18 @@ buildscript {
             exclude(group = "com.google.protobuf")
         }
     }
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    ratchetFrom = "origin/main"
+
+    kotlin {
+        target("**/*.kt")
+        ktfmt(libs.versions.ktfmt.get()).kotlinlangStyle()
+    }
+}
+
+task<Exec>("gitConfigApply") {
+    executable = "git"
+    args("config", "--local", "include.path", "../.gitconfig")
 }
